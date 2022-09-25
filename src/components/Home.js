6554cb48bb8170ba454871357f891e4d134ae469
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-09-25 16:00:10 +0300

    Add Home component markup and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
+import "./Home.scss";
+import Background from "../assets/background.jpg";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <img className="background" src={Background} alt="background" />
+            <div className="filter">
+                <h1>AMD Shop</h1>
+                <Link to="/shop">
+                    <button>Catalog</button>
+                </Link>
+            </div>
+        </>
+    );
 };
 
 export default Home;
